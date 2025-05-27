24d5eef1fccea83f87b8bcd91a6d1146a81d1d50
NureShpakDenys <denys.shpak@nure.ua>
2025-05-27 10:57:46 +0300

    removed needless lines

--- Lab5/pzpi-22-3-shpak-denys-lab5/global-app/server/api.go
+++ Lab5/pzpi-22-3-shpak-denys-lab5/global-app/server/api.go
@@ -5,18 +5,15 @@ import (
 	"net/http"
 )
 
-// APIError represents a generic error with an HTTP status code and a message.
 type APIError struct {
 	Status int
 	Msg    string
 }
 
-// Error implements the error interface for APIError.
 func (e *APIError) Error() string {
 	return e.Msg
 }
 
-// NewAPIError constructs a new APIError with formatting.
 func NewAPIError(status int, format string, args ...interface{}) error {
 	return &APIError{
 		Status: status,
@@ -24,12 +21,10 @@ func NewAPIError(status int, format string, args ...interface{}) error {
 	}
 }
 
-// NoFieldsFoundError represents a 404 error when a specific field or object is not found.
 type NoFieldsFoundError struct {
 	APIError
 }
 
-// NewNoFieldsFoundError returns a new NoFieldsFoundError for the given object name.
 func NewNoFieldsFoundError(objName string) error {
 	return &NoFieldsFoundError{
 		APIError: APIError{
